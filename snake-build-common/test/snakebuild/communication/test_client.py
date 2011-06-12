f56f1f5a80fa81487d07796946d83f39efd77387
Mathias Weber <mathew.weber@gmail.com>
2011-06-12 17:21:34 +0200

    added test cases for the Client test
    added only test for private methods but they are not yet implemented.

--- snake-build-common/test/snakebuild/communication/test_client.py
+++ snake-build-common/test/snakebuild/communication/test_client.py
@@ -2,6 +2,9 @@
 # Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
 
 import unittest
+import copy
+
+from snakebuild.communication import Client
 
 
 class TestClient(unittest.TestCase):
@@ -9,7 +12,47 @@ class TestClient(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_create(self):
-        ''' TODO'''
+    def test_prepare_sjson_data(self):
+        ''' Test the private method _preapre_sjson_data. This method must
+            create a message string with the full header and the json string
+            to send.
+        '''
+        data = {'cmd': 'test', 'parameters': {'list': [1,2,3], 'element': 12,
+            'others': 'Elephant'}}
+        orig = copy.deepcopy(data)
+
+        cli = Client('', 22222)
+        message = cli._prepare_sjson_data(data)
         res = 10
         self.assertTrue(res is None)
+
+    def test_parse_sjson_data(self):
+        ''' Test the private method _parse_sjson_data. This method expectes to 
+            receive a socket and reads from this a sjson data string.
+            This tests creates a mockup of a socket to provide the data.
+        '''
+        data = {'cmd': 'test', 'parameters': {'list': [1,2,3], 'element': 12,
+            'others': 'Elephant'}}
+        orig = copy.deepcopy(data)
+
+        cli = Client('', 22222)
+        # TODO
+#        dummy_sock = DummySocket()
+#        dummy_sock.add_data(message_string)
+#        message = cli._parse_sjson_data(dummy_sock)
+
+    def _test_receive(self):
+        ''' Test the private method _receive. This method expects a socket to
+            receive the data. With this test we provide a dummy socket that
+            provides the expected data.
+        '''
+        data = {'cmd': 'test', 'parameters': {'list': [1,2,3], 'element': 12,
+            'others': 'Elephant'}}
+        orig = copy.deepcopy(data)
+
+        cli = Client('', 22222)
+        # TODO
+#        dummy_sock = DummySocket()
+#        dummy_sock.add_data(message_string)
+#        message = cli._receive(dummy_sock)
+
