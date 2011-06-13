899a91b8f99a2c3f99a7b2e6652e2455ff71ac0f
Mathias Weber <mathew.weber@gmail.com>
2011-06-13 18:47:06 +0200

    implemented the server side.
    No tests for this yet

---
+++ snake-build-common/snakebuild/communication/messagehandler.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
+
+import SocketServer
+import json
+import logging
+
+log = logging.getLogger('snakebuild.communication.messagehandler')
+
+
+class MessageHandler(SocketServer.BaseRequestHandler):
+    ''' Receive the messags from the client and handle the received command
+        if the command is defined within the server.commands dictionary.
+        If not log the message and ignore it. (Send error message)
+    '''
+    def handle(self):
+        ''' Handle the data sent and check what to with it. '''
+        data = self.request.recv(1)
+        if not len(data) == 1:
+            return
+        if ord(data[0]) == 0x61:
+            self._parse_sjson_request()
+        else:
+            log.error('The message type received is not supported. got: %x' %
+                    data[0])
+            return
+
+    def _parse_sjson_request(self):
+        ''' Get a sjson request and call the appropriate command if available.
+        '''
+        length_data = self.request.recv(4)
+        if not len(length_data) == 4:
+            log.error('The message received did not return 4 bytes for the '
+                    'length of th message: Only got: %d' % len(length_data))
+            return
+        length = ((ord(data[0]) << 24) + (ord(data[1]) << 16) +
+                (ord(data[2]) << 8) + ord(data[3]))
+
+        data = self.request.recv(length)
+        if not len(data) == length:
+            log.error('Wrong length of data received: Expected %d but got %d' %
+                    (length, len(data)))
+            return
+        try:
+            cmd = json.loads(data)
+        except ValueError:
+            log.error('Could not parse the received data. Not a valid json '
+                    'string.')
+            return
+        if not 'cmd' in cmd:
+            log.error("The message received did not have a 'cmd' key.")
+            return
+        if not 'parameters' in cmd:
+            log.error("The message received did not have a 'parameters' key.")
+            return
+
+        self._handle_cmd(cmd['cmd'], cmd['parameters'],
+                self.request.server.commands)
+
+    def _handle_cmd(self, cmd, parameters, commands):
+        ''' Handle the given command if it is specified within the commands.
+
+            @param cmd: The command as a string
+            @param parameters: The paramters for the command
+            @param commands: The dictionary with the suported commands
+        '''
+        cmd = cmd.lower()
+        if cmd in commands:
+            return commands[cmd][0](cmd, parameters)
+        else:
+            cmd_list = dict((k.lower(), v) for k, v in commands.iteritems())
+            if cmd.lower() in cmd_list:
+                return cmd_list[cmd][0](cmd, parameters)
+            else:
+                log.error("The requested command '%d' is not supported by the "
+                        "given server implementation." % cmd)
+                return
--- snake-build-common/snakebuild/communication/server.py
+++ snake-build-common/snakebuild/communication/server.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
 
+import threading
+import socket
+import time
+import logging
+import SocketServer
+
+from snakebuilder.communication import MessageHandler
+
+log = logging.getLogger('snakebuild.communication.messagehandler')
+
 
 class ServerCommunicationException(BaseException):
     ''' The exception that gets trown on an error during the communication with
@@ -8,42 +18,75 @@ class ServerCommunicationException(BaseException):
     '''
 
 
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    ''' Get a multithreaded socket server. '''
+
+
 class Server(object):
     ''' This class is simple wrapper class for a server class which handles
-        sjson messages. The parsing of the messages is done. The class must
-        only provide a list with commands and the commands to call on this
-        commands.
+        sjson messages. The parsing of the messages is done. The user of this
+        class must provide a list with commands and the commands to call on
+        this commands.
 
-        # TODO
-        Message Types:
-
-        sjson
-        -----
-        Identifier byte = 'a' or 0x61
-          | |    | ... |
-           |   |    \----> The data bytes as a json string (must be complete)
-           |   |           the length must comply with the length given in the
-           |   |           length field.
-           |   \---------> The length field (4 bytes) specifying the length of
-           |               the message. Must be encoded as big-endian.
-           \-------------> The idenifier byte (must be 'a' or as value 0x61)
-
-        The data type to provide for sending a SJSON request must be a
-        dictionary/list or other basic type that can be transformed to a json
-        string without special handling. The answer for a sjson message is
-        a dictionary/list or other basic type as well.
-
-        No other types are currently supported but others might follow.
+        For a description of the protocol see the Snake-Build-Dev
+        documentation.
     '''
     # this defines the known message types
     SJSON, UNKNWON = range(2)
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, commands):
         ''' Create a server object with the given host and port. The server
             does not start listening until the run method is called.
 
+            The commands dictionary has the commands that are supported as keys
+            and as the value a tuple with the first element a callable which
+            takes two parameters. 1. command 2. parameters
+
             @param host: The host address (as a string)
             @param port: The network port to use for the connection (as int)
+            @param commands: A dictionary with all the supported commands and
+                    the methods to call if such a command get received.
         '''
         self.host = host
         self.port = port
+        self.commands = commands
+        self.server = None
+        self.server_running = True
+
+    def run(self):
+        ''' Start the server. This method will not return until the server
+            gets stoped.
+        '''
+        srvr = threading.Thread(target=self.run_server)
+        srvr.start()
+
+        while self.server_running:
+            try:
+                time.sleep(1)
+            except (KeyboardInterrupt, SystemExit):
+                break
+
+        self.server_running = False
+        if self.server is not None:
+            self.server.shutdown()
+
+    def run_server(self):
+        ''' This method starts listening on the network host:port for clients.
+            This method does not return until the server gets stopped.
+        '''
+        self.server = None
+        while self.server is None and self.server_running:
+            try:
+                self.server = ThreadedTCPServer((self.host, self.port),
+                        MessageHandler)
+            except socket.error:
+                log.warning('Could not open network connection, will try again'
+                        ' within a few seconds.')
+                time.sleep(2)
+        log.info('Server started.')
+
+        self.server.commands = self.commands
+        if self.server_running:
+            self.server.serve_forever()
+
+        log.info('Shutdown the server.')
