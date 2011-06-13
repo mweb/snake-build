cc310de4675e8ebfa52e4f8cec2a3d77f46d0162
Mathias Weber <mathew.weber@gmail.com>
2011-06-13 16:20:08 +0200

    added testcases for the server
    no test implemented since the server implementation is still missing.

--- snake-build-common/test/snakebuild_test/communication/create.py
+++ snake-build-common/test/snakebuild_test/communication/create.py
@@ -4,10 +4,12 @@
 import unittest
 
 from test_client import TestClient
+from test_server import TestServer
 
 
 def suite():
     ''' Get the test suite for the communication snakebuild classes. '''
     client = unittest.TestLoader().loadTestsFromTestCase(TestClient)
+    server = unittest.TestLoader().loadTestsFromTestCase(TestServer)
 
-    return unittest.TestSuite([client])
+    return unittest.TestSuite([client, server])
---
+++ snake-build-common/test/snakebuild_test/communication/test_server.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
+
+import unittest
+
+from snakebuild.communication.client import Server, \
+        ServerCommunicationException
+
+#from test_helpers.dummysocket import DummySocket
+
+
+class TestServer(unittest.TestCase):
+    ''' The unit test for the snake build communication Server class. '''
+    def setUp(self):
+        pass
