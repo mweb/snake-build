87e9bd59a8dfb883d6db07e5c97dbe30737d170f
Mathias Weber <mathew.weber@gmail.com>
2011-06-16 22:34:01 +0200

    Implemented the testcase for the Config class
    Implemented the default value test case including testing the doc string
    of each config value.

--- snake-build-common/test/snakebuild_test/common/test_config.py
+++ snake-build-common/test/snakebuild_test/common/test_config.py
@@ -34,13 +34,51 @@ class TestConfig(unittest.TestCase):
                         "description": "Dolphins"},
                     "fifth": {"default": False, "type": "bool",
                         "description": "G'Gugvunnts and Vl'hurgs"}}}
-        f = open('data/test_data.txt', 'w')
-        f.write(json.dumps(data))
+        dfd = open('data/test_data.txt', 'w')
+        dfd.write(json.dumps(data))
 
     def test_init_config(self):
-        ''' Initialize the config object with default values. '''
+        ''' Initialize the config object with default values and check values.
+        '''
         config = Config()
         with self.assertRaises(ConfigValueException):
             config.init_default_config('data/test_config.txt')
         config.init_default_config('data/test_data.txt')
 
+        self.assertTrue(config.application_name == 'snakebuile_test')
+
+        self._check_value('client', 'first', 'Aldebarans', str, 'Start',
+                'Start')
+        self._check_value('client', 'second', 'Altairians', unicode, 'Stop',
+                'Stop')
+        self._check_value('client', 'third', 'Amoeboid Zingatularians', int,
+                12, 12)
+        self._check_value('client', 'forth', 'Bartledanians', float, 12.2,
+                12.2)
+        self._check_value('client', 'fifth', 'Belcerebons', bool, True, True)
+
+        self._check_value('server', 'first', 'Betelgeusians', str, 'End', 
+                'End')
+        self._check_value('server', 'second', 'Blagulon Kappans', unicode, 
+                'Accelerate', 'Accelerate')
+        self._check_value('server', 'third', 'Dentrassis', int, -12, -12)
+        self._check_value('server', 'forth', 'Dolphins', float, 3.3333, 3.3333)
+        self._check_value('server', 'fifth', "G'Gugvunnts and Vl'hurgs", bool,
+                False, False)
+
+    def _check_value(self, section, key, edesc, etype, edefault, evalue):
+        ''' Check if the given config value has the expected values.
+            @param section: The section to check
+            @param key: The key to check
+            @param edesc: Expected Description
+            @param etype: Expected type
+            @param edefault: Expected default value
+            @param evalue: Expected value
+        '''
+        value = Config().get_s(section, key)
+        self.assertTrue(type(value) == etype)
+        self.assertTrue(value == evalue)
+        desc, ctype, default = Config().get_description(section, key)
+        self.assertTrue(default == edefault)
+        self.assertTrue(desc == edesc)
+        self.assertTrue(ctype == etype)
