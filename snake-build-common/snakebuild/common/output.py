bb1a411421a65f2f8f29f220e87b395efecce32a
Mathias Weber <mathew.weber@gmail.com>
2011-06-18 16:48:39 +0200

    Added output functionality within common
    The output module provides different functionality for pretty printing.

---
+++ snake-build-common/snakebuild/common/output.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
+''' The function within this file support the output to the console file or any
+    other source which requires pretty printed output.
+'''
+
+import os
+
+__LINE_LENGTH = None
+
+
+def format_message(value, indent="", line_length=None, first_ident=None):
+    ''' Return a string with newlines so that the given string fits into this
+        line length. At the start of the line the indent is added. This can
+        be used for commenting the message out within a file or to indent your
+        text.
+
+        If the line length is set to None then the width is expected to be the
+        width of the current console.
+        All \\t will be replaced with 4 spaces.
+
+        @param value: The string to get as a commented multiline comment.
+        @param ident: The ident to use for printing or charcter to put in front
+        @param line_length: The length of the line to fill. (default None)
+        @param first_ident: The first ident might be shorter. If None then the
+                first line uses the normal ident as the rest of the string.
+
+        @return: The string with newlines
+    '''
+    if indent.find('\t'):
+        indent = indent.replace('\t', '    ')
+    line_length = get_line_lengt(line_length) - len(indent)
+
+    result = []
+    if first_ident is None:
+        first_ident = indent
+    tmp = first_ident
+    first = True
+    for ele in value.split(' '):
+        if ele.find('\t'):
+            ele = ele.replace('\t', '    ')
+        if len(ele) + len(tmp) > line_length:
+            result.append(tmp)
+            tmp = '%s%s' % (indent, ele)
+        else:
+            if first:
+                tmp = "%s%s" % (tmp, ele)
+                first = False
+            else:
+                tmp = "%s %s" % (tmp, ele)
+
+    return "\n".join(result)
+
+
+def get_line_lengt(line_length):
+    ''' Get the line length of a terminal or the given line lenght if not None
+
+        @param line_length: The line length to chekc if None then the terminal
+                width is returned
+        @return the length of the line to use for the current terminal.
+    '''
+    if line_length is not None:
+        return line_length
+
+    global __LINE_LENGTH
+    if __LINE_LENGTH is None:
+        __LINE_LENGTH = _get_terminal_size()[0] - 1
+    return __LINE_LENGTH
+
+
+def _get_terminal_size():
+    ''' Get the size of the current terminal.
+
+        @return: (width, height)
+    '''
+    def ioctl_get_win_size(fdr):
+        ''' try to get the size of a terminal
+            @param fdr: The filedescriptor id
+        '''
+        try:
+            import fcntl
+            import termios
+            import struct
+            crr = struct.unpack('hh',
+                    fcntl.ioctl(fdr, termios.TIOCGWINSZ, '1234'))
+        except:
+            return None
+        return crr
+    crr = (ioctl_get_win_size(0) or ioctl_get_win_size(1) or
+            ioctl_get_win_size(2))
+    if not crr:
+        try:
+            fdr = os.open(os.ctermid(), os.O_RDONLY)
+            crr = ioctl_get_win_size(fdr)
+            os.close(fdr)
+        except:
+            pass
+    if not crr:
+        try:
+            crr = (env['LINES'], env['COLUMNS'])
+        except:
+            # safe default value (we take 80 here)
+            crr = (25, 80)
+
+    return int(crr[1]), int(crr[0])
