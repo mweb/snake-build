d3d9c3fbf1ee4af7939372e62c73ec418630a7a7
Mathias Weber <mathew.weber@gmail.com>
2011-06-23 16:11:22 +0200

    added test files

---
+++ snake-build-resource-server/test/snakebuild_test/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
+''' The snakebuild test suite '''
+
+from create import suite
---
+++ snake-build-resource-server/test/snakebuild_test/create.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
+''' Create the snakebuild_test suite '''
+
+import unittest
+
+
+def suite():
+    ''' Get the test suite for the common snakebuild classes. '''
+#    common_test = common.suite()
+
+    return unittest.TestSuite([])
