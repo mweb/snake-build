3d8f519c79bea62c2197181eccccfd8a4d7e7f2f
Mathias Weber <mathew.weber@gmail.com>
2011-06-24 18:58:00 +0200

    got the sb-resourceserver binary working again.

--- snakebuild/resourceserver/__init__.py
+++ snakebuild/resourceserver/__init__.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
-''' The snakebuild_resourceserver package. The package for the snake build
+''' The snakebuild.resourceserver package. The package for the snake build
     resource server.
 '''
-
-__author__ = """Mathias Weber <mathew.weber@gmail.com>"""
-__version__ = 0.0
-__docformat__ = 'plaintext'
 
 from server import start_server
 from commandlineparser import parse_command_line
