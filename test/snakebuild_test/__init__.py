ebdb412d1b9d9fe699532b090b26c73653063d82
Mathias Weber <mathew.weber@gmail.com>
2011-06-28 22:10:48 +0200

    changed the license to gpl3

--- test/snakebuild_test/__init__.py
+++ test/snakebuild_test/__init__.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
+#
 # Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
+#
+# This file is part of Snake-Build.
+#
+# Snake-Build is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Snake-Build is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Snake-Build.  If not, see <http://www.gnu.org/licenses/>
 ''' The snakebuild test suite '''
 
 from create import suite
