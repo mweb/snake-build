7e35046bcf1727a64adf7396dc7f1995e1d4277a
Mathias Weber <mathew.weber@gmail.com>
2011-08-31 21:35:23 +0200

    added the empty ResourceManager class

---
+++ snakebuild/resourceserver/resource/resourcemanager.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006-2011 Mathias Weber <mathew.weber@gmail.com>
+#
+# This file is part of Snake-Build.
+#
+# Snake-Build is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Snake-Build is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Snake-Build.  If not, see <http://www.gnu.org/licenses/>
+''' The file contains a class which is used as the resource manager. This
+    instance will hold all the resources to be managed and will handle the
+    scheduling of request to the different resources to have a good usage.
+'''
+
+class ResourceManager(object):
+    pass
