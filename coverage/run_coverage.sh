4175c0aca16eeeef90881630c223c9c8a6d6c46b
Mathias Weber <mweb@compartor.(none)>
2011-11-22 21:47:01 +0100

    remove htmlcov before creating new coverage results

--- coverage/run_coverage.sh
+++ coverage/run_coverage.sh
@@ -4,6 +4,10 @@ if [ -e ".coverage" ]; then
     rm .coverage
 fi
 
+if [ -e "htmlcov" ]; then
+    rm -r htmlcov
+fi
+
 python-coverage run ../test/run_tests.py
 python-coverage html
 firefox htmlcov/index.html
