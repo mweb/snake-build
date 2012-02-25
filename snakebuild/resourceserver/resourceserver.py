059eb3b78596f15438b02f6f9297e387a7a3afc0
Mathias Weber <mathew.weber@gmail.com>
2012-02-25 20:56:56 +0100

    fixed a print of a not i18n error message.

--- snakebuild/resourceserver/resourceserver.py
+++ snakebuild/resourceserver/resourceserver.py
@@ -38,8 +38,8 @@ def start_server(options, arguments, config):
     '''
     stop = False
     if not (len(arguments) == 0 or len(arguments) == 1):
-        print("Could not start the resource server illegal arguments. "
-                "Expected None or one argument but got: {0:d}".format(
+        print(_("Could not start the resource server illegal arguments. "
+                "Expected None or one argument but got: {0:d}").format(
                 len(arguments)))
         return False
 
