455c111ed58b912a6e1739f15d7d468cdfc2bb71
Mathias Weber <mathew.weber@gmail.com>
2012-03-18 20:33:23 +0100

    Added an automatic generated git repository for testing

--- test/snakebuild_test/common/test_versioneddir.py
+++ test/snakebuild_test/common/test_versioneddir.py
@@ -19,15 +19,24 @@
 ''' The unit test for the versioned directory class '''
 
 import unittest
+import os
+import shutil
+import subprocess
 
 import snakebuild.common.versioneddir as vd
+from snakebuild.common.appdirs import tmp_data_dir
 
 
 class TestVersionedDir(unittest.TestCase):
     ''' The unit test for the snake build common versioned dir classes. '''
     def setUp(self):
         ''' Setup the test case. '''
-        pass
+        self.tempdir = os.path.join(tmp_data_dir(), 'snakebuild_test')
+        if os.path.isdir(self.tempdir):
+            shutil.rmtree(self.tempdir)
+
+        os.makedirs(self.tempdir)
+        _init_git_repo(self.tempdir)
 
     def test_init(self):
         ''' Test the initialisation of the versioned directory.
@@ -37,5 +46,67 @@ class TestVersionedDir(unittest.TestCase):
     def test_git_commands(self):
         ''' Test the internal git command methods.
         '''
-        versioned = vd.VersionedGitDir('/tmp/testrepos')
+        versioned = vd.VersionedGitDir(self.tempdir)
         self.assertTrue(versioned._gitr('branch') == 0)
+
+
+def _init_git_repo(path):
+    ''' Initialize a git repository and populate it with some data to test it.
+
+        @param path: The path to the empty directory to init the git repos
+    '''
+    prevdir = os.getcwd()
+    os.chdir(path)
+
+    subprocess.check_call(['git', 'init'])
+    _add_git_file('one', '1')
+    _add_git_file('two', '2')
+    _add_tag('v1.0', 'first tag')
+    _create_branch('v1.x')
+    _add_git_file('three', '3')
+    _add_tag('v1.1', 'branch tag')
+    _checkout_branch('master')
+    _add_git_file('four', '4')
+    _add_tag('v2.0', 'add 2.0er tag')
+
+    os.chdir(prevdir)
+
+
+def _create_branch(name):
+    ''' Create a new branch
+
+        @param name: the name of the branch
+    '''
+    subprocess.check_call(['git', 'checkout', '-b', name])
+
+
+def _checkout_branch(name):
+    ''' checkout a given branch
+
+        @param name: the name of the branch to get
+    '''
+    subprocess.check_call(['git', 'checkout', name])
+
+
+def _add_tag(tagname, tagcomment):
+    ''' tag the current state.
+
+        @param tagname: The name of the tag
+        @param tagcomment: The comment to use for the tag
+    '''
+    subprocess.check_call(['git', 'tag', '-a', tagname,
+            '-m {0}'.format(tagcomment)])
+
+
+def _add_git_file(filename, content):
+    ''' Add a new file to the git repository and add the given content.
+        @param filename: The name of the file to add
+        @param content: The content of the file
+    '''
+    newfile = open(filename, 'w')
+    newfile.write(content)
+    newfile.close()
+
+    subprocess.check_call(['git', 'add', filename])
+    subprocess.check_call(['git', 'commit',
+        '-m added file {0}'.format(filename)])
