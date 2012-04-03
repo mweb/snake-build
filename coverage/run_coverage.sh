684e1812974d5f426233c03560506b18ee323bf2
Mathias Weber <mathew.weber@gmail.com>
2012-04-03 21:21:57 +0200

    silence the stdout output of the tests.

--- coverage/run_coverage.sh
+++ coverage/run_coverage.sh
@@ -8,6 +8,7 @@ if [ -e "htmlcov" ]; then
     rm -r htmlcov
 fi
 
-python-coverage run ../test/run_tests.py
+python-coverage run ../test/run_tests.py > out.txt
 python-coverage html --omit="/usr/share/*,/*test*"
 firefox htmlcov/index.html
+rm out.txt
