1ba3c0255bd0dc504aafbf336aa8c9c733a03d8d
Mathias Weber <mathew.weber@gmail.com>
2012-04-09 19:09:37 +0200

    integrated the integration tests into the coverage results
    use the parameter long for the run_coverage script to run it.
    The long currently only works if /tmp exists and is writable
    (linux/unix bases systems)

--- coverage/run_coverage.sh
+++ coverage/run_coverage.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -e
+
+LONG=0
+if [ $# -eq 1 ]; then
+    if [ $1 = "long" ]; then
+        LONG=1
+    fi
+fi
+
 if [ -e ".coverage" ]; then
     rm .coverage
 fi
@@ -9,6 +18,17 @@ if [ -e "htmlcov" ]; then
 fi
 
 python-coverage run ../test/run_tests.py > out.txt
+# run long tests?
+if [ ${LONG} -eq 1 ]; then
+    cd ../test/integration_test/
+    ./test_resources.sh /tmp/ cov
+    cd -
+    mv .coverage .coverage.1
+    cp ../test/integration_test/server/.coverage .coverage.2
+    cp ../test/integration_test/client/.coverage .coverage.3
+    python-coverage combine
+fi
+
 python-coverage html --omit="/usr/share/*,/*test*"
 firefox htmlcov/index.html
 rm out.txt
