923f4ee94e8c1070890d393578a49954581da190
Mathias Weber <mathew.weber@gmail.com>
2012-06-13 20:21:58 +0200

    add support for pip installed coverage
    Added variable to call python-coverage to be able to use it with
    a python-coverage installed with pip into my virtualenv.

--- coverage/run_coverage.sh
+++ coverage/run_coverage.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+#COVERAGE=python-coverage
+COVERAGE=coverage
+
 LONG=0
 if [ $# -eq 1 ]; then
     if [ $1 = "long" ]; then
@@ -17,7 +20,7 @@ if [ -e "htmlcov" ]; then
     rm -r htmlcov
 fi
 
-python-coverage run ../test/run_tests.py > out.txt
+${COVERAGE} run ../test/run_tests.py > out.txt
 # run long tests?
 if [ ${LONG} -eq 1 ]; then
     cd ../test/integration_test/
@@ -26,9 +29,9 @@ if [ ${LONG} -eq 1 ]; then
     mv .coverage .coverage.1
     cp ../test/integration_test/server/.coverage .coverage.2
     cp ../test/integration_test/client/.coverage .coverage.3
-    python-coverage combine
+    ${COVERAGE} combine
 fi
 
-python-coverage html --omit="/usr/share/*,/*test*"
+${COVERAGE} html --omit="/usr/share/*,/*test*"
 firefox htmlcov/index.html
 rm out.txt
